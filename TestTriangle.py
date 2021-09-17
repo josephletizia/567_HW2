db0c12ceb11e92591afc99027cb2c0b98f36a5b8
Joseph Letizia <metsjjl@gmail.com>
2021-09-17 17:28:36 -0400

    update triangle test

--- TestTriangle.py
+++ TestTriangle.py
@@ -7,6 +7,10 @@ The primary goal of this file is to demonstrate a simple unittest implementation
 @author: rk
 """
 
+#Joseph Letizia
+#SSW 567
+#HW2
+
 import unittest
 
 from Triangle import classifyTriangle
@@ -25,6 +29,22 @@ class TestTriangles(unittest.TestCase):
         
     def testEquilateralTriangles(self): 
         self.assertEqual(classifyTriangle(1,1,1),'Equilateral','1,1,1 should be equilateral')
+
+    def testScaleneTriangles(self):
+        self.assertEqual(classifyTriangle(2, 5, 6), 'Scalene', '2,5,6 is a scalene triangle')
+
+    def testIsocelesTriangles(self):
+        self.assertEqual(classifyTriangle(2, 2, 3), 'Isoceles', '2,2,3 is a isoceles triangle')
+
+    def testValidation(self):
+        self.assertEqual(classifyTriangle(201, 2, 3), 'InvalidInput', 'all ints must be 200 or less')
+        self.assertEqual(classifyTriangle(1,2,0), 'InvalidInput', 'all ints must be greater than 0')
+        self.assertEqual(classifyTriangle(1,-2,3), 'InvalidInput', 'all ints must be greater than 0')
+        self.assertEqual(classifyTriangle("a", 1, 1), 'InvalidInput', 'all inputs must be integers')
+
+    def testNotATriangle(self):
+        self.assertEqual(classifyTriangle(20, 5, 6), 'NotATriangle', 'sum of two sides must be greater than the third side')   
+        self.assertEqual(classifyTriangle(11, 5, 6), 'NotATriangle', 'sum of two sides must be greater than the third side')   
 
 if __name__ == '__main__':
     print('Running unit tests')
